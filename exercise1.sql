6ecd641d7a0dbbe736e76a02d6d4d5c1b2851d2c
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-11 12:55:20 +0000

    done 1

--- exercise1.sql
+++ exercise1.sql
@@ -43,3 +43,48 @@ WHERE name LIKE "プリン%";
 SELECT *
 FROM purchases
 WHERE NOT character_name = "にんじゃわんこ";
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムがNULLであるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE price is NULL ;
+
+-- 「FROM purchases」のあとにコードを追加し、categoryカラムが「食費」かつcharacter_nameカラムが「ひつじ仙人」であるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE category = "食費" AND character_name = "ひつじ仙人";
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムを基準に降順に並び替えたデータを取得してください
+
+SELECT *
+FROM purchases
+ORDER BY price DESC ;
+
+-- 「FROM purchases」のあとにコードを追加し、取得結果が「最大5件」となるようにデータを取得してください。
+
+SELECT *
+FROM purchases
+LIMIT 5 ;
+
+-- 「FROM purchases」のあとにコードを追加し、purchased_atカラムが「2017-08-01以前」のデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE purchased_at <= "2017-08-01";
+
+-- 「FROM purchases」のあとにコードを追加し、nameカラムが「プリン」を含むデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE name LIKE "%プリン%" ;
+
+/*
+「FROM purchases」のあとにコードを追加し、
+NOT演算子を用いてcharacter_nameカラムが「にんじゃわんこ」でないデータを取得してください。
+*/
+
+SELECT *
+FROM purchases
+WHERE NOT character_name = "にんじゃわんこ" ;
--- exercise2.sql
+++ exercise2.sql
@@ -31,3 +31,48 @@ WHERE name LIKE "%プリン";
 SELECT *
 FROM purchases
 WHERE NOT name LIKE "%プリン%";
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムがNULLでないデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE price is NOT NULL ;
+
+-- 「FROM purchases」のあとにコードを追加し、categoryカラムが「食費」またはcharacter_nameカラムが「にんじゃわんこ」であるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE category = "食費" OR character_name = "にんじゃわんこ";
+
+-- 「WHERE character_name = "にんじゃわんこ"」のあとにコードを追加し、priceカラムを基準に昇順に並び替えたデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE character_name = "にんじゃわんこ"
+ORDER BY PRICE ASC ;
+
+-- 「WHERE character_name = "にんじゃわんこ"」のあとにコードを追加し、取得結果が「最大10件」となるようにデータを取得してください。
+
+SELECT *
+FROM purchases
+WHERE character_name = "にんじゃわんこ"
+LIMIT 10 ;
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムがNULLであるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE price IS NULL ; 
+
+-- 「FROM purchases」のあとにコードを追加し、categoryカラムが「食費」かつcharacter_nameカラムが「ひつじ仙人」であるデータを取得してください
+
+SELECT *
+FROM purchases
+WHERE category = "食費" AND character_name = "ひつじ仙人" ;
+
+-- 「FROM purchases」のあとにコードを追加し、priceカラムを基準に「降順に並び替えた」データを、最大で「5件」取得してください
+
+SELECT *
+FROM purchases
+ORDER BY price DESC
+LIMIT 5 ;
